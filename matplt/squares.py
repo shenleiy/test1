198aa2906a965d18653332c6373d5d6a80d96978
shenleiy <1491217808@qq.com>
2021-03-25 16:18:19 +0800

    delect

--- matplt/squares.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-y_squares = list(range(1, 1000))
-x_squares = [x ** 2 for x in y_squares]
-
-plt.scatter(x_squares, y_squares, s=10)
-plt.axis([0, 1100000, 0, 1100])
-plt.show()
